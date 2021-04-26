ee31515a7416d54083b9eee0513db528841c1b81
Justin Marin <jumarin@protonmail.com>
2021-04-26 16:41:07 -0400

    Adventure mode complete. Changed all methods to void

--- Program.cs
+++ Program.cs
@@ -60,16 +60,16 @@ namespace FirstBankOfSuncoast
             while (keepRunning)
             {
                 Console.WriteLine("What would you like to do?");
-                Console.WriteLine("(D)eposit, (W)ithdraw, (B)alance, (H)istory, (Q)uit");
+                Console.WriteLine("(D)eposit \n(W)ithdraw \n(B)alance  \n(H)istory  \n(T)ransfer  \n(Q)uit");
                 var userChoice = Console.ReadLine().ToLower();
 
                 switch (userChoice)
                 {
                     case "d" or "deposit":
-                        transactions.Add(Deposit());
+                        Deposit(transactions);
                         break;
                     case "w" or "withdraw":
-                        transactions.Add(Withdraw(transactions));
+                        Withdraw(transactions);
                         break;
                     case "b" or "balance":
                         Console.WriteLine($"Checking Account Balance: ${Balance(transactions, "Checking")}");
@@ -77,6 +77,9 @@ namespace FirstBankOfSuncoast
                         break;
                     case "h" or "history":
                         History(transactions);
+                        break;
+                    case "t" or "transfer":
+                        Transfer(transactions);
                         break;
                     case "q" or "quit":
                         keepRunning = false;
@@ -102,6 +105,105 @@ namespace FirstBankOfSuncoast
             Console.WriteLine("Goodbye!");
         }
 
+        private static double Balance(List<Transaction> transactions, string accountType)
+        {
+            // Pull objects from List filtered by Transaction.Account result.
+            var cList = transactions.Where(line => line.Account == accountType).ToList();
+            double aBalance = 0; // Create temporary balance sum. 
+            foreach (var line in cList)
+            {
+                if (line.Action == "Deposit")
+                    aBalance += line.Amount;
+                else if (line.Action == "Withdraw")
+                    aBalance -= line.Amount;
+            }
+
+            return aBalance;
+        }
+
+        // Method to Deposit.
+        private static void Deposit(List<Transaction> transactions)
+        {
+            Transaction deposit = new Transaction();
+            deposit.Action = "Deposit";
+            string answer = "";
+            bool keepAsking = true;
+            while (keepAsking)
+            {
+                answer = PromptForString("In which Account would you like to make a Deposit: (C)hecking or (S)avings?\n").ToLower();
+                if (answer == "c" || answer == "checking")
+                {
+                    deposit.Account = "Checking";
+                    keepAsking = false;
+                }
+                else if (answer == "s" || answer.Contains("saving"))
+                {
+                    deposit.Account = "Savings";
+                    keepAsking = false;
+                }
+                else
+                    Console.WriteLine("Sorry, I don't understand.");
+            }
+
+            deposit.Amount = PromptForDub($"How much are you Despositing in your {deposit.Account} Account?\n");
+
+            Console.WriteLine($"You Deposited ${deposit.Amount} in your {deposit.Account} Account on {deposit.When()}");
+
+            transactions.Add(deposit);
+        }
+
+        // Method to Withdraw.
+        private static void Withdraw(List<Transaction> transactions)
+        {
+            Transaction withdraw = new Transaction();
+            withdraw.Action = "Withdraw";
+            double aBalance = 0; // Create temporary balance amount. 
+            string answer = "";
+            bool keepAsking = true;
+            while (keepAsking)
+            {
+                answer = PromptForString("Select an Account to Withdraw from: (C)hecking or (S)avings?\n").ToLower();
+                if (answer == "c" || answer == "checking")
+                {
+                    withdraw.Account = "Checking";
+                    keepAsking = false;
+                }
+                else if (answer == "s" || answer.Contains("saving"))
+                {
+                    withdraw.Account = "Savings";
+                    keepAsking = false;
+                }
+                else
+                    Console.WriteLine("Sorry, I don't understand.");
+            }
+
+            // Method to check Balance of an Account.
+            aBalance = Balance(transactions, withdraw.Account); // Run Balance check to avoid overDraft.
+            Console.WriteLine($"Your {withdraw.Account} Account Balance is ${aBalance}");
+            if (aBalance == 0)
+            {
+                Console.WriteLine("You cannot Withdraw from an Account with 0 dollars!");
+                return;
+            }
+
+            bool overDraft = true;
+            while (overDraft)
+            {
+                withdraw.Amount = PromptForDub($"How much are you Withdrawing from your {withdraw.Account} Account?\n");
+                if (withdraw.Amount > aBalance)
+                {
+                    Console.WriteLine($"You can only Withdraw up to ${aBalance}");
+                }
+                else
+                    overDraft = false;
+            }
+
+            Console.WriteLine($"You Withdrew {withdraw.Amount} from your {withdraw.Account} Account on {withdraw.When()}");
+
+            transactions.Add(withdraw);
+        }
+
+        // Method to see History of Transactions for an Account.
         private static void History(List<Transaction> transactions)
         {
             var answer = "";
@@ -135,90 +237,50 @@ namespace FirstBankOfSuncoast
                     Console.WriteLine("Sorry, I don't understand.");
             }
         }
-
-        private static double Balance(List<Transaction> transactions, string accountType)
+        // Adventure Mode: Add the ability to transfer funds from my checking to my saving (and vice versa).
+        // Creates 2 transactions, one Withdraw() then one Deposit(). 
+        // Repeats most code from Withdraw(), then Deposits() same amount in other Account. Suffers from wet code. 
+        private static void Transfer(List<Transaction> transactions)
         {
-            // Pull objects from List filtered by Transaction.Account result.
-            var cList = transactions.Where(line => line.Account == accountType).ToList();
-            double aBalance = 0; // Create temporary balance sum. 
-            foreach (var line in cList)
-            {
-                if (line.Action == "Deposit")
-                    aBalance += line.Amount;
-                else if (line.Action == "Withdraw")
-                    aBalance -= line.Amount;
-            }
-
-            return aBalance;
-        }
-
-        private static Transaction Deposit()
-        {
-            Transaction deposit = new Transaction();
-            deposit.Action = "Deposit";
-            string answer = "";
-            bool keepAsking = true;
-            while (keepAsking)
-            {
-                answer = PromptForString("In which Account would you like to make a Deposit: (C)hecking or (S)avings?\n").ToLower();
-                if (answer == "c" || answer == "checking")
-                {
-                    deposit.Account = "Checking";
-                    keepAsking = false;
-                }
-                else if (answer == "s" || answer.Contains("saving"))
-                {
-                    deposit.Account = "Savings";
-                    keepAsking = false;
-                }
-                else
-                    Console.WriteLine("Sorry, I don't understand.");
-            }
-
-            deposit.Amount = PromptForDub($"How much are you Despositing in your {deposit.Account} Account?\n");
-
-            Console.WriteLine($"You Deposited ${deposit.Amount} in your {deposit.Account} Account on {deposit.When()}");
-
-            return deposit;
-        }
-
-        private static Transaction Withdraw(List<Transaction> transactions)
-        {
-            Transaction withdraw = new Transaction();
-            withdraw.Action = "Withdraw";
+            Transaction transferW = new Transaction();
+            Transaction transferD = new Transaction();
+            transferW.Action = "Withdraw";
+            transferD.Action = "Deposit";
             double aBalance = 0; // Create temporary balance amount. 
             string answer = "";
             bool keepAsking = true;
             while (keepAsking)
             {
-                answer = PromptForString("Select an Account to Withdraw from: (C)hecking or (S)avings?\n").ToLower();
+                answer = PromptForString("Select an Account to Transfer from: (C)hecking or (S)avings?\n").ToLower();
                 if (answer == "c" || answer == "checking")
                 {
-                    withdraw.Account = "Checking";
+                    transferW.Account = "Checking";
+                    transferD.Account = "Savings";
                     keepAsking = false;
                 }
                 else if (answer == "s" || answer.Contains("saving"))
                 {
-                    withdraw.Account = "Savings";
+                    transferW.Account = "Savings";
+                    transferD.Account = "Checking";
                     keepAsking = false;
                 }
                 else
                     Console.WriteLine("Sorry, I don't understand.");
             }
 
-            aBalance = Balance(transactions, withdraw.Account); // Run Balance check to avoid overDraft.
-            Console.WriteLine($"Your {withdraw.Account} Account Balance is ${aBalance}");
+            aBalance = Balance(transactions, transferW.Account); // Run Balance check to avoid overDraft.
+            Console.WriteLine($"Your {transferW.Account} Account Balance is ${aBalance}");
             if (aBalance == 0)
             {
-                Console.WriteLine("You cannot Withdraw from an Account of 0 value!");
-                return withdraw; // This should probably be Try/catch exception thingie. 
+                Console.WriteLine("You cannot Transfer from an Account with 0 dollars!");
+                return;
             }
 
             bool overDraft = true;
             while (overDraft)
             {
-                withdraw.Amount = PromptForDub($"How much are you Withdrawing from your {withdraw.Account} Account?\n");
-                if (withdraw.Amount > aBalance)
+                transferW.Amount = PromptForDub($"How much are you Transferring from your {transferW.Account} Account to your {transferD.Account}?\n");
+                if (transferW.Amount > aBalance)
                 {
                     Console.WriteLine($"You can only Withdraw up to ${aBalance}");
                 }
@@ -226,12 +288,15 @@ namespace FirstBankOfSuncoast
                     overDraft = false;
             }
 
-            Console.WriteLine($"You Withdrew {withdraw.Amount} from your {withdraw.Account} Account on {withdraw.When()}");
+            transactions.Add(transferW);
+            transferD.Amount = transferW.Amount;
+            transactions.Add(transferD);
 
-            return withdraw;
+            Console.WriteLine($"You transferred ${transferW.Amount} from your {transferW.Account} Account to your {transferD.Account} Account on {transferW.When()}");
         }
 
-        private static double PromptForDub(string prompt) // Method when asking for numbers. 
+        // Method when asking for numbers. 
+        private static double PromptForDub(string prompt)
         {
             Console.Write(prompt);
             var userInput = Console.ReadLine();
@@ -244,7 +309,8 @@ namespace FirstBankOfSuncoast
             return numberInput;
         }
 
-        private static string PromptForString(string prompt) // Method when asking for strings. 
+        // Method when asking for strings. 
+        private static string PromptForString(string prompt)
         {
             Console.Write(prompt);
             var userInput = Console.ReadLine();
